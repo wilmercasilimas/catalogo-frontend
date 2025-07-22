7f04c4a7f92fa7ec2e2c032047c9279c0b452ae3
Wilmer Casilimas Sanchez <wilmercasilimas@gmail.com>
2025-07-21 21:04:20 -0400

    Prepara despliegue inicial en Vercel

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,9 @@
+// vite.config.ts
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import path from "path";
 
+// Esto garantiza compatibilidad en todos los entornos
 const __dirname = path.resolve();
 
 export default defineConfig({
@@ -11,13 +13,4 @@ export default defineConfig({
       "@": path.resolve(__dirname, "src"),
     },
   },
-  // ❌ Eliminar esta sección:
-  // server: {
-  //   proxy: {
-  //     "/api": {
-  //       target: "http://localhost:4000",
-  //       changeOrigin: true,
-  //     },
-  //   },
-  // },
 });
